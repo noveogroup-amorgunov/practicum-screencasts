a0fce9254bc8eddd0a0ea585296f207b5debc964
amorgunov <amorgunov@samokat.ru>
2022-04-28 11:53:12 +0300

    Fix url in apiMock.ts

--- sprint-4-tests/src/tests/apiMock.ts
+++ sprint-4-tests/src/tests/apiMock.ts
@@ -2,7 +2,7 @@ import { setupServer } from 'msw/node';
 import { rest } from 'msw'
 
 const handlers = [
-  rest.post('https://ya-praktikum.tech/api/v2/auth/logout', (req, res, ctx) => {
+  rest.post(`${process.env.API_ENDPOINT}/auth/logout`, (req, res, ctx) => {
     console.log('Call logout endpoind');
 
     return res(ctx.status(200));
